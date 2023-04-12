e3a544e5b3991999365fe8e76c5ff860d39666d9
PietroSavini <savinipietro@gmail.com>
2023-04-12 14:15:03 +0200

    END OF PROJECT

---
+++ src/components/appHero.vue
@@ -0,0 +1,31 @@
+<script>    
+    import AppTitle from "./appTitle.vue";
+    export default {
+    name: 'AppHero',
+    components:{
+        AppTitle,
+    },
+    data(){
+        return{
+           
+        }
+    }
+}
+</script>
+
+<template>
+    <div class= "container">
+        <AppTitle/>
+    </div>
+</template>
+
+<style>
+    .container{
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+</style>
---
+++ src/components/appTitle.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        name: 'AppTitle',
+        data(){
+            return{
+                title: "Vite",
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1 class="title">la mia prima app con {{ title }}</h1>
+</template>
+
+<style>
+    h1:hover{
+        background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
+        filter: drop-shadow(0 0 0.75rem rgb(190, 34, 10));
+        transform: scale(1.5);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+</style>
